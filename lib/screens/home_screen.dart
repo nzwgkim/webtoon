cd4dd96eaacb3ea0a14e98a2cf128f86a56e9aa1
Woody <nz.wgkim@gmail.com>
2023-01-06 17:12:17 +1300

    using initState to get data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,13 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 import 'package:webtoon/services/api_services.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    waitForWebToons();
+  }
 
   final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -17,7 +41,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: const Text(
-          "어늘의 웹툰",
+          "Today's Webtoon",
           style: TextStyle(
             fontSize: 24,
           ),
@@ -43,21 +67,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-
-  // ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
-  //   return ListView.separated(
-  //     scrollDirection: Axis.horizontal,
-  //     itemCount: snapshot.data!.length,
-  //     padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-  //     itemBuilder: (context, index) {
-  //       var webtoon = snapshot.data![index];
-  //       return Webtoon(
-  //         title: webtoon.title,
-  //         thumb: webtoon.thumb,
-  //         id: webtoon.id,
-  //       );
-  //     },
-  //     separatorBuilder: (context, index) => const SizedBox(width: 40),
-  //   );
-  // }
 }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -4,18 +4,31 @@ import 'package:http/http.dart' as http;
 import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
+
+    // final url = Uri.https('$baseUrl', '$today');
+    // alternative way???
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
+      // String get body
+      // package:http/src/response.dart
+      // print(response.body);
+
+      //dynamic jsonDecode(String source, {Object? Function(Object?, Object?)? reviver})
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
+        // final oneWebToon = WebtoonModel.fromJson(webtoon);
+        // print(oneWebToon.title);
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
+      // print(webtoonInstances.length);
       return webtoonInstances;
     }
     throw Error();
