e456c1d7cb6dd1cb63e6449bae80b6fe3d90392b
Woody <nz.wgkim@gmail.com>
2023-01-07 05:26:07 +1300

    ListView.separated를 적용

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,28 +26,31 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                var current = snapshot.data![index];
-                return Text(current.title);
-              },
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-            );
+            // return ListView.builder(
+            //   itemBuilder: (context, index) {
+            //     print(index);
+            //     var current = snapshot.data![index];
+            //     return Text(current.title);
+            //   },
+            //   scrollDirection: Axis.horizontal,
+            //   itemCount: snapshot.data!.length,
+            // );
+
             // return ListView(
             //   children: [
             //     for (var webtoon in snapshot.data!) Text(webtoon.title)
             //   ],
             // );
-            // return ListView.separated(
-            //   scrollDirection: Axis.horizontal,
-            //   itemCount: snapshot.data!.length,
-            //   itemBuilder: (context, index) {
-            //     var webtoon = snapshot.data![index];
-            //     return Text(webtoon.title);
-            //   },
-            //   separatorBuilder: (context, index) => const SizedBox(width: 20),
-            // );
+
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            );
           }
           return const Center(
             child: CircularProgressIndicator(),
